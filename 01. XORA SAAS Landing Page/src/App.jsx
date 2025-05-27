82299e7ba82fa7bd2967fff913993d7f315e8a1e
Neeraj <neeraj05042001@gmail.com>
2025-05-27 06:09:55 +0530

    Header Completed

---
+++ 01. XORA SAAS Landing Page/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Sections/Header";
+import Hero from "./Sections/Hero";
+const App = () => {
+  return (
+    <>
+      <main className="overflow-hidden">
+        <Header/>
+        <Hero/>
+
+      </main>
+      
+    </>
+  );
+};
+
+export default App;
---
+++ 01. XORA SAAS Landing Page/src/Sections/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-400">
+      <div></div>
+    </section>
+  );
+};
+
+export default Hero;
